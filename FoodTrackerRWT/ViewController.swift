0e2d69954993f21bf599ef5512326d8e5b180900
Richard Tanner <rwt07a@lib-301-ts-9310.local>
2019-02-15 08:14:40 -0600

    Initial Commit

---
+++ FoodTrackerRWT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTrackerRWT
+//
+//  Created by Richard Tanner on 2/15/19.
+//  Copyright © 2019 Richard Tanner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
