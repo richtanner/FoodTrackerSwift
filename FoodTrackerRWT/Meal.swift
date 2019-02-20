9c85a03f2d21ab42adde19da758788918cce269c
Richard Tanner <rwt07a@lib-301-ts-9310.local>
2019-02-20 08:47:22 -0600

    Data model and Unit Tests.  Woot.

---
+++ FoodTrackerRWT/Meal.swift
@@ -0,0 +1,41 @@
+//
+//  Meal.swift
+//  FoodTrackerRWT
+//
+//  Created by Richard Tanner on 2/20/19.
+//  Copyright © 2019 Richard Tanner. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        // Initialization should fail if there is no name or if the rating is negative.
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+    
+}
+
+
